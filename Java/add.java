2ddab22522ca1fc926825ddedc03da7210ecdc00
supriyasandhyan <47947363+supriyasandhyan@users.noreply.github.com>
2021-07-09 13:26:27 +0530

    Add files via upload
    Program to add two numbers

---
+++ Java/add.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Add
+{
+public static void main(String args[])
+{
+Scanner sc= new Scanner(System.in);System.out.print("Enter the first number: ");
+int x=sc.nextInt();
+
+System.out.print("Enter the second number: ");
+int y=sc.nextInt();
+
+System.out.print("Addition of both number is: ");
+int z=x+y;
+
+System.out.print(z);
+}
+}
