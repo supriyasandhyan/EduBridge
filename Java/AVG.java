790b39f1830a675f761239dc76d34a97a9e22729
supriyasandhyan <47947363+supriyasandhyan@users.noreply.github.com>
2021-09-16 00:09:58 +0530

    Update AVG.java

--- Java/AVG.java
+++ Java/AVG.java
@@ -1,33 +1,20 @@
-import java.util.*;
-class AVG
-{   
-public static void main(String[] args)  
-{  
-int a, b, c, d, e, sum, avg;   
-Scanner sc = new Scanner(System.in);  
-   
-System.out.println("Enter the value of a: ");  
-a=sc.nextInt();  
-      
-System.out.println("Enter the value of b: ");  
-b=sc.nextInt();  
-
-System.out.println("Enter the value of c: ");  
-c=sc.nextInt();  
-
-System.out.println("Enter the value of d: ");  
-d=sc.nextInt();  
-
-System.out.println("Enter the value of e: ");  
-e=sc.nextInt();  
-
-sum=a+b+c+d+e;
-System.out.println("sum of number is: " +sum);
-
-avg=sum/5;
-System.out.println("Average of number is: " +avg);
-             
-}   
-                  
-}    
-  
+import java.util.*;  
+public class AVG {  
+   public static void main(String[] args)  
+    {  
+      int n, count = 1;   
+      float  F, avg, sum=0;   
+      Scanner sc = new Scanner(System.in);     
+      System.out.println("Numbers: ");  
+      n = sc.nextInt();  
+      while (count <= n)   
+             {   
+                  System.out.println("Enter the "+count+" number: ");  
+                  F = sc.nextInt();  
+                  sum += F;   
+                  ++count;   
+             }   
+                  avg = sum/n;   
+        System.out.println("The Average is: " +avg);  
+    }    
+}  
